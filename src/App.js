47136ce4da0bee7be78b5ab0f6442bd2356b15fc
Chazaray Robinson <chaarayrobinson@Chazarays-MacBook-Pro.local>
2019-09-16 15:50:54 -0400

    complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from "react-router-dom"
 
 const App = () => (
   <div>
     <Navigation />
+    <Route exact path="/" componet={Home} />
+    <Route exact path="/About" componet={About} />
+    <Route exact path="/Contact" componet={Contact} />
+
   </div>
 );
 
